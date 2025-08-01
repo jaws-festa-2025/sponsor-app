37803fca27b162dfdfb043c20d0052c48254f71f
Masayuki Katoh <kato@phalanxware.com>
2025-08-01 10:27:28 +0900

    herokuにあわせて修正

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,5 @@
 class Organization < ApplicationRecord
-  has_many :sponsorships
+  has_many :sponsorships, dependent: :destroy
   validates :name, presence: true
   validates :domain, presence: true
 
